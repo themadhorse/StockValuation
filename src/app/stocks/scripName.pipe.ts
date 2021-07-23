a6e90ac5226c0d16edd8fca290f47116385e8229
HarshK <harshshailkul@gmail.com>
2021-07-23 17:41:19 +0530

    Small bug in removeNS pipe: FIXED

--- src/app/stocks/scripName.pipe.ts
+++ src/app/stocks/scripName.pipe.ts
@@ -3,6 +3,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({name: 'removeNS'})
 export class ScripNamePipe implements PipeTransform {
   transform(scrip: string){
-    return scrip.substring(0, scrip.indexOf('.NS'));
+    if(scrip.indexOf('.NS') !== -1)
+      return scrip.substring(0, scrip.indexOf('.NS'));
+    return scrip;
   }
 }
